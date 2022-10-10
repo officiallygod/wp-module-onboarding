e07bb901d2187931e31ef6ede19eac1fdf8c224f
abhijitb <abhijit.bhatnagar@gmail.com>
2022-10-10 17:57:08 +0530

    added allowed list to the wp_config class
    modified:   includes/WP_Config.php

--- includes/WP_Config.php
+++ includes/WP_Config.php
@@ -1,6 +1,9 @@
 <?php
 namespace NewfoldLabs\WP\Module\Onboarding;
 
+use NewfoldLabs\WP\Module\Onboarding\Data\Options;
+use NewfoldLabs\WP\Module\Onboarding\Data\Config;
+
 /**
  * Access and Modify WordPress Configuration (wp-config.php).
  */
@@ -11,8 +14,15 @@ class WP_Config {
 	  */
 	 protected $wp_config;
 
+	 /**
+	  * @var array Store the list of allowed WP config options
+	  */
+	protected $allowed_options;
+
 	function __construct() {
 		 $this->wp_config = new \WPConfigTransformer( ABSPATH . 'wp-config.php' );
+		 // fetch all the options that are allowed to be added or updated
+		 $this->allowed_options = array_merge( Options::get_initialization_options(), Config::get_wp_config_initialization_constants(), Options::get_all_options());
 	}
 
 	 /**
@@ -24,7 +34,10 @@ class WP_Config {
 	  * @return boolean
 	  */
 	public function add_constant( $name, $value ) {
-		 return $this->wp_config->add( 'constant', $name, $value, array( 'raw' => true ) );
+		if(!array_key_exists($name, $this->allowed_options)) {
+			return false;
+		}
+		return $this->wp_config->add( 'constant', $name, $value, array( 'raw' => true ) );
 	}
 
 	 /**
@@ -36,7 +49,10 @@ class WP_Config {
 	  * @return boolean
 	  */
 	public function update_constant( $name, $value ) {
-		 return $this->wp_config->update( 'constant', $name, $value, array( 'raw' => true ) );
+		if(!array_key_exists($name, $this->allowed_options)) {
+			return false;
+		}
+		return $this->wp_config->update( 'constant', $name, $value, array( 'raw' => true ) );
 	}
 
 	 /**
